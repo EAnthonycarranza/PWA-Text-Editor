c11b1d7c0d30bbbebb337d7de57559d72022ef3d
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2023-09-06 11:22:00 -0500

    Update webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -69,7 +69,7 @@ module.exports = () => {
     },
     output: {
       filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve('client', 'dist'),
     },
   };
 };
